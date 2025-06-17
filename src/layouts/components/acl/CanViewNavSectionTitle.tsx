95ee44cec434005e672e14423e9f431c471bde4a
Rama Dhaniansyah <dhaniansyahr@gmail.com>
2025-06-17 19:24:58 +0700

    fix: update build error

--- src/layouts/components/acl/CanViewNavSectionTitle.tsx
+++ src/layouts/components/acl/CanViewNavSectionTitle.tsx
@@ -1,11 +1,12 @@
 // ** React Imports
-import { ReactNode, useContext } from 'react'
+import { ReactNode } from 'react'
 
 // ** Component Imports
-import { AbilityContext } from 'src/layouts/components/acl/Can'
 
 // ** Types
 import { NavSectionTitle } from 'src/@core/layouts/types'
+import { Actions, Subjects } from 'src/configs/acl'
+import { useAbility } from 'src/context/AbilityContext'
 
 interface Props {
   children: ReactNode
@@ -17,12 +18,12 @@ const CanViewNavSectionTitle = (props: Props) => {
   const { children, navTitle } = props
 
   // ** Hook
-  const ability = useContext(AbilityContext)
+  const ability = useAbility()
 
   if (navTitle && navTitle.auth === false) {
     return <>{children}</>
   } else {
-    return ability && ability.can(navTitle?.action, navTitle?.subject) ? <>{children}</> : null
+    return ability && ability.can(navTitle?.action as Actions, navTitle?.subject as Subjects) ? <>{children}</> : null
   }
 }
 
