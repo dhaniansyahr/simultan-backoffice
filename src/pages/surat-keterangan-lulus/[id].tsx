ae53b8f9228fdb455f343cd2e0340e88550a6230
alifannaufallyatha <athaalifan@gmail.com>
2025-08-09 01:32:25 +0700

    Create [id].tsx

---
+++ src/pages/surat-keterangan-lulus/[id].tsx
@@ -0,0 +1,7 @@
+import Detail from 'src/libs/surat-keterangan-lulus/containers/Detail'
+
+const SuratKeteranganLulusDetailPage = () => {
+  return <Detail />
+}
+
+export default SuratKeteranganLulusDetailPage
