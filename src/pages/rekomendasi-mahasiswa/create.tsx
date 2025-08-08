be529eba69bae5f77d0fd96a8e45d37795483386
alifannaufallyatha <athaalifan@gmail.com>
2025-08-09 01:31:50 +0700

    Create create.tsx

---
+++ src/pages/rekomendasi-mahasiswa/create.tsx
@@ -0,0 +1,12 @@
+// import CreateRekomendasiMahasiswa from 'src/libs/rekomendasi-mahasiswa/containers/CreateContainer'
+
+// const CreateRekomendasiMahasiswaPage = () => {
+//   return <CreateRekomendasiMahasiswa />
+// }
+
+// CreateRekomendasiMahasiswaPage.acl = {
+//   subject: 'REKOMENDASI_MAHASISWA',
+//   action: 'CREATE'
+// }
+
+// export default CreateRekomendasiMahasiswaPage
