5af53e33ab7397e676796bbe0b38bfa0016b076f
alifannaufallyatha <athaalifan@gmail.com>
2025-08-09 01:31:53 +0700

    Create index.tsx

---
+++ src/pages/rekomendasi-mahasiswa/create/index.tsx
@@ -0,0 +1,19 @@
+// import React from 'react'
+// import { NextPage } from 'next'
+// import AclGuard from 'src/@core/components/auth/AclGuard'
+// import RekomendasiMahasiswaCreateContainer from 'src/libs/rekomendasi-mahasiswa/containers/RekomendasiMahasiswaCreateContainer'
+
+// const aclProperties = {
+//   action: 'CREATE' as const,
+//   subject: 'REKOMENDASI_MAHASISWA' as const
+// }
+
+// const CreateRekomendasiMahasiswa: NextPage = () => {
+//   return (
+//     <AclGuard aclAbilities={aclProperties} guestGuard={false} authGuard={true}>
+//       <RekomendasiMahasiswaCreateContainer />
+//     </AclGuard>
+//   )
+// }
+
+// export default CreateRekomendasiMahasiswa
