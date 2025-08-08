d6196c0ea6ff34acc189ba0d7a1cb79cac25cd62
alifannaufallyatha <athaalifan@gmail.com>
2025-08-09 01:32:05 +0700

    Create rekomendasiMahasiswaSlice.ts

---
+++ src/stores/rekomendasi-mahasiswa/rekomendasiMahasiswaSlice.ts
@@ -0,0 +1,19 @@
+// import { createSlice } from '@reduxjs/toolkit'
+
+// const initialState = {
+//   refresher: 0
+// }
+
+// const rekomendasiMahasiswaSlice = createSlice({
+//   name: 'rekomendasiMahasiswa',
+//   initialState,
+//   reducers: {
+//     setRefresher: state => {
+//       state.refresher = state.refresher + 1
+//     }
+//   }
+// })
+
+// export const { setRefresher } = rekomendasiMahasiswaSlice.actions
+
+// export default rekomendasiMahasiswaSlice.reducer
